7fc7b9e12bd7134481007610e51a6ef7862f1a29
Puru-Rana <pururana24@gmail.com>
2024-01-04 05:04:39 -0500

    Email Verification

--- routes.ts
+++ routes.ts
@@ -3,7 +3,7 @@
  * These routes do not require authentication
  * @type {string[]}
  */
-export const publicRoutes = ["/"];
+export const publicRoutes = ["/", "/auth/new-verification"];
 
 /**
  * An array of routes that are used for authentications
@@ -17,7 +17,7 @@ export const authRoutes = ["/auth/login", "/auth/register", "/auth/error"];
  * The prefix for API authecation routes
  * Routes that start with this prefix are used for API
  * authentication purpose
- * @type {string}
+* @type {string}
  */
 export const apiAuthPrefix = "/api/auth";
 
